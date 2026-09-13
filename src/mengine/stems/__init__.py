35d8a203dddd77ec5ce7b840f3b6d3e780c56cc2
BlackMamba <neocyber1@gmail.com>
2026-09-13 06:23:50 -0600

    feat(stems): export stem analysis contract

--- src/mengine/stems/__init__.py
+++ src/mengine/stems/__init__.py
@@ -1,3 +1,4 @@
+from mengine.stems.analysis import StemAnalysis, analyze_stems
 from mengine.stems.demucs import DemucsError, DemucsSeparator
 
-__all__ = ["DemucsError", "DemucsSeparator"]
+__all__ = ["DemucsError", "DemucsSeparator", "StemAnalysis", "analyze_stems"]
