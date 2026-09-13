e849b1d15d73d7e0a317fc0b266362b1b801a204
BlackMamba <neocyber1@gmail.com>
2026-09-13 06:23:43 -0600

    feat(backends): export ACE-Step adapter

--- src/mengine/backends/__init__.py
+++ src/mengine/backends/__init__.py
@@ -1,4 +1,5 @@
+from mengine.backends.ace_step import AceStepAPIBackend, AceStepError
 from mengine.backends.base import GeneratorBackend
 from mengine.backends.dummy import DummyGenerator
 
-__all__ = ["GeneratorBackend", "DummyGenerator"]
+__all__ = ["AceStepAPIBackend", "AceStepError", "GeneratorBackend", "DummyGenerator"]
